e2d4444fa64f6be73dd83e4ba49b64684050857f
Andrius <andriusjavait@gmail.com>
2017-03-29 17:25:11 +0100

    Created MainServlet.java this is just empty servlet with empty doGet() , and in DD we create <servlet> and <servlet-mapping>

---
+++ Chapter13_2/src/main/java/com/andrius/project/Chapter13_2/servlet/MainServlet.java
@@ -0,0 +1,12 @@
+package com.andrius.project.Chapter13_2.servlet;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class MainServlet extends HttpServlet {
+	
+	public void doGet(HttpServletRequest req, 
+			HttpServletResponse res)throws ServletException, IOException{
+		
+	}
+}
